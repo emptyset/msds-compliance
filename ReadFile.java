13ba8acaf7df59831777db5167fafc99cd4ac420
Pranav <Pranav@Pranav-PC.(none)>
2012-09-15 13:07:36 -0400

    i reformatted the file

--- ReadFile.java
+++ ReadFile.java
@@ -1,32 +1,34 @@
+
 import java.io.*;
 
-public class ReadFile{
-    public static void main(String[] args){
+public class ReadFile {
+
+    public static void main(String[] args) {
         try {
-			
-			FileReader input = new FileReader(args[0]);
-			BufferedReader bufRead = new BufferedReader(input);
-			
+
+            FileReader input = new FileReader(args[0]);
+            BufferedReader bufRead = new BufferedReader(input);
+
             String line; 	// String that holds current file line
             int count = 0;	// Line number of count 
-            
+
             line = bufRead.readLine();
             count++;
-            
-            while (line != null){
-                System.out.println(count+": "+line);
+
+            while (line != null) {
+                System.out.println(count + ": " + line);
                 line = bufRead.readLine();
                 count++;
             }
-            
-            bufRead.close();
-			
-        }catch (ArrayIndexOutOfBoundsException e){
-			System.out.println("Usage: java ReadFile filename\n");			
 
-		}catch (IOException e){
+            bufRead.close();
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Usage: java ReadFile filename\n");
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
     }
 }
