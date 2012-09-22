bdb84586d9390d828b1a8d90205c092aeca67519
Pranav <Pranav@Pranav-PC.(none)>
2012-09-22 09:13:55 -0400

    I read the notepad file of chemicals and added each of the chemicals into an expandable Arraylist. I also think i understand how to retrieve information from a website, but I don't know how to input information (input the chemical information).

---
+++ testProgram.java
@@ -0,0 +1,68 @@
+
+import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
+
+public class testProgram {
+
+    public static void main(String[] args) {
+
+        try {
+
+            FileReader input = new FileReader(args[0]);
+            BufferedReader bufRead = new BufferedReader(input);
+
+            String line; 	// String that holds current file line
+            int lineCount = 0;	// Line number of count
+            ArrayList chemicalList = new ArrayList();    // List of all the chemicals. Line number is index + 1
+
+            line = bufRead.readLine();
+            chemicalList.add(line);
+            lineCount++;
+
+            while (line != null) {
+                line = bufRead.readLine();
+                chemicalList.add(line);
+                lineCount++;
+            }
+
+            bufRead.close();
+
+
+
+
+            URL url = new URL("http://google.com");
+            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+
+
+
+            // SEARCH CHEMICAL
+
+
+
+
+            InputStream in = httpCon.getInputStream();
+            OutputStream out = new FileOutputStream("file.txt");
+            out = new BufferedOutputStream(out);
+            byte[] buf = new byte[8192];
+            int len = 0;
+            while ((len = in.read(buf)) != -1) {
+                out.write(buf, 0, len);
+            }
+            out.close();
+
+
+
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Usage: java ReadFile filename\n");
+
+
+        } catch (IOException e) {
+            System.out.println("error! u suck at this");
+            e.printStackTrace();
+
+
+        }
+    }
+}
