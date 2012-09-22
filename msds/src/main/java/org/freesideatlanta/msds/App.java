9e1157b5e9913ba1efeecebff65d3cd9372d91b2
Pranav <Pranav@Pranav-PC.(none)>
2012-09-22 12:17:18 -0400

    a

---
+++ msds/src/main/java/org/freesideatlanta/msds/App.java
@@ -0,0 +1,13 @@
+package org.freesideatlanta.msds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
