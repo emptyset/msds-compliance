460747aad62720fd265bcaa054b022e6a518485c
Pranav <Pranav@Pranav-PC.(none)>
2012-09-22 14:10:53 -0400

    attempt at using apache http client

--- msds/src/main/java/org/freesideatlanta/msds/App.java
+++ msds/src/main/java/org/freesideatlanta/msds/App.java
@@ -1,5 +1,8 @@
 package org.freesideatlanta.msds;
 
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.*;
+import org.apache.commons.httpclient.params.HttpMethodParams;
 import java.util.ArrayList;
 import java.io.*;
 import java.net.*;
@@ -10,12 +13,14 @@ public class App {
 
         try {
 
+
+
             FileReader input = new FileReader(args[0]);
             BufferedReader bufRead = new BufferedReader(input);
 
             String line; 	// String that holds current file line
             int lineCount = 0;	// Line number of count
-            ArrayList chemicalList = new ArrayList();    // List of all the chemicals. Line number is index + 1
+            ArrayList<String> chemicalList = new ArrayList<String>();    // List of all the chemicals. Line number is index + 1
 
             line = bufRead.readLine();
             chemicalList.add(line);
@@ -23,7 +28,7 @@ public class App {
 
             while (line != null) {
                 line = bufRead.readLine();
-                chemicalList.add(line);
+                chemicalList.add(line.trim());
                 lineCount++;
             }
 
@@ -32,26 +37,41 @@ public class App {
 
 
 
-            URL url = new URL("http://google.com");
-            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
 
 
 
-            // SEARCH CHEMICAL
+            HttpClient client = new HttpClient();
+            String URLhere = "http://www.commonchemistry.org/search.aspx";
+
+
+            for (String a : chemicalList) {
+                PostMethod amethod = new PostMethod(URLhere);
+                amethod.addParameter("terms", a);
 
 
 
+                int statusCode = client.executeMethod(amethod);
 
-            InputStream in = httpCon.getInputStream();
-            OutputStream out = new FileOutputStream("file.txt");
-            out = new BufferedOutputStream(out);
-            byte[] buf = new byte[8192];
-            int len = 0;
-            while ((len = in.read(buf)) != -1) {
-                out.write(buf, 0, len);
+                if (statusCode != HttpStatus.SC_OK) {
+                    System.err.println("Method failed: " + amethod.getStatusLine());
+                }
+
+
+
+                InputStream in = amethod.getResponseBodyAsStream();
+
+
+
+                String name = a.replaceAll("\\s", "_") + ".txt";
+                OutputStream out = new FileOutputStream(name);
+                out = new BufferedOutputStream(out);
+                byte[] buf = new byte[8192];
+                int len = 0;
+                while ((len = in.read(buf)) != -1) {
+                    out.write(buf, 0, len);
+                }
+                out.close();
             }
-            out.close();
-
 
 
 
