809cdba2c863b8bd676bc2d0731268d98bac393b
Alan Fay <alan@faysoftware.com>
2012-10-07 11:52:04 -0400

    Added MsdsCatalog skeleton class

---
+++ msds/src/main/java/org/freesideatlanta/msds/MsdsCatalog.java
@@ -0,0 +1,23 @@
+package org.freesideatlanta.msds;
+
+import org.apache.http.*;
+import org.apache.http.client.*;
+import org.apache.http.params.*;
+import org.apache.http.impl.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.util.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
+
+public class MsdsCatalog {
+
+
+}
